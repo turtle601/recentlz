c50da4bdeec331219f38acb2ff0a8ce14f86a6a6
turtle601 <turtle601@naver.com>
2024-05-06 12:08:40 +0900

    📦 chore: emotion css 및 reset css 설정 [#1]

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Global } from '@emotion/react';
+import { globalStyle } from '@/styles/global';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<Global styles={globalStyle} />
 		<App />
 	</React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@components", replacement: "/src/components" },
-    ]
-  }
-})
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: ['@emotion/babel-plugin'],
+			},
+		}),
+	],
+	resolve: {
+		alias: [{ find: '@', replacement: './src' }],
+	},
+});
