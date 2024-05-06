8cfd93b155a079c9652bf9b7fb6213db62dbf333
turtle601 <turtle601@naver.com>
2024-05-06 13:25:03 +0900

    🐛 fix: 절대 경로 오류로 인한 설정 수정 [#3]

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
 			},
 		}),
 	],
+
 	resolve: {
-		alias: [{ find: '@', replacement: './src' }],
+		alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
 	},
 });
