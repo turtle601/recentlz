a2da9faa7c20536d63c4a1fd887479c09fa1ed9a
turtle601 <turtle601@naver.com>
2024-05-08 12:57:15 +0900

    📦 chore: express + mysql template 설정 [#07]

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,30 @@
-import app from './app';
+import express, { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
+
+const app = express();
+const PORT = process.env.PORT;
 
 require('dotenv').config();
 
-const port = process.env.PORT;
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME || '',
+  process.env.DATABASE_USERNAME || '',
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+  }
+);
 
-app.listen(port, () => {});
+app.get('/', (req: Request, res: Response) => {
+  res.json('Hello World!');
+});
+
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('DB 연결 성공!');
+  } catch (err) {
+    console.log('DB 연결 X', err);
+  }
+});
