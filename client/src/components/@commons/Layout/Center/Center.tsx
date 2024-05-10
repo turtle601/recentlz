6e9818a7a1dade89f46dfac186f03225cf814599
turtle601 <turtle601@naver.com>
2024-05-10 15:29:49 +0900

    ✨ feat: Center 컴포넌트 구현 [#13]

---
+++ client/src/components/@commons/Layout/Center/Center.tsx
@@ -0,0 +1,31 @@
+import { ElementType } from 'react';
+
+import Flex from '@/components/@commons/Layout/Flex/Flex';
+
+import type { CenterProps } from '@/components/@commons/Layout/Center/Center.type';
+import type { PolymorpicProps } from '@/@types/polymorpic';
+
+const Center = <T extends ElementType = 'div'>({
+	as,
+	children,
+	direction,
+	styles = {},
+	...attribute
+}: PolymorpicProps<T, CenterProps>) => {
+	const Element = as || 'div';
+
+	return (
+		<Flex
+			as={Element}
+			direction={direction}
+			justify="center"
+			align="center"
+			styles={styles}
+			{...attribute}
+		>
+			{children}
+		</Flex>
+	);
+};
+
+export default Center;
