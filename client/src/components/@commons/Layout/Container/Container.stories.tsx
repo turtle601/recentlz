a65d6f74df897d466c3455d314264d1b5fb49ebe
turtle601 <turtle601@naver.com>
2024-05-10 15:30:04 +0900

    ✨ feat: Container 컴포넌트 구현 [#13]

---
+++ client/src/components/@commons/Layout/Container/Container.stories.tsx
@@ -0,0 +1,41 @@
+import { css } from '@emotion/react';
+import { color } from '@/styles/theme';
+
+import Container from './Container';
+
+import type { StoryObj } from '@storybook/react';
+
+const meta = {
+	title: 'Container',
+	component: Container,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const boxStyle = css({
+	width: '600px',
+	height: '40px',
+	backgroundColor: color.blue500,
+	'& + &': {
+		marginTop: '10px',
+	},
+});
+
+export const Default: Story = {
+	render: () => {
+		return (
+			<Container minWidth="500px">
+				<div css={boxStyle}></div>
+				<div css={boxStyle}></div>
+				<div css={boxStyle}></div>
+				<div css={boxStyle}></div>
+				<div css={boxStyle}></div>
+				<div css={boxStyle}></div>
+			</Container>
+		);
+	},
+};
---
+++ client/src/components/@commons/Layout/Container/Container.tsx
@@ -0,0 +1,32 @@
+import { ElementType } from 'react';
+
+import { getContainerStyle } from '@/components/@commons/Layout/Container/Container.style';
+
+import type { PolymorpicProps } from '@/@types/polymorpic';
+import type { ContainerProps } from '@/components/@commons/Layout/Container/Container.type';
+
+function Container<T extends ElementType = 'div'>({
+	as,
+	maxWidth = 'none',
+	minWidth = 'none',
+	styles = {},
+	children,
+	...attribute
+}: PolymorpicProps<T, ContainerProps>) {
+	const Element = as || 'div';
+
+	return (
+		<Element
+			css={getContainerStyle({
+				minWidth,
+				maxWidth,
+				styles,
+			})}
+			{...attribute}
+		>
+			{children}
+		</Element>
+	);
+}
+
+export default Container;
