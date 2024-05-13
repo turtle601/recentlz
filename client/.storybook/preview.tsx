4cc145f5a09cf699563c8cc9b57d4d8b7e2e030b
turtle601 <turtle601@naver.com>
2024-05-13 20:17:17 +0900

    📦 chore: tanstack-query + msw 설치 및 셋팅 [#17]

--- client/.storybook/preview.tsx
+++ client/.storybook/preview.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Global } from '@emotion/react';
 
+import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { initialize, mswDecorator, mswLoader } from 'msw-storybook-addon';
+
+import { worker } from '../src/mocks/browser';
+import { handlers } from '../src/mocks/handler';
+
 import { globalStyle } from '../src/styles/global';
 
-import type { Preview } from '@storybook/react';
+const queryClient = new QueryClient();
+
+if (typeof global.process === 'undefined') {
+  worker.start();
+}
+
+initialize();
 
 export const preview: Preview = {
   parameters: {
@@ -13,6 +26,10 @@ export const preview: Preview = {
         date: /Date$/i,
       },
     },
+    staticDirs: ['./public'],
+    msw: {
+      handlers: { ...handlers },
+    },
   },
 };
 
@@ -20,11 +37,14 @@ export const decorators = [
   (Story) => (
     <>
       <div>
-        <Global styles={globalStyle} />
-        <Story />
+        <QueryClientProvider client={queryClient}>
+          <Global styles={globalStyle} />
+          <Story />
+        </QueryClientProvider>
       </div>
     </>
   ),
+  mswDecorator,
 ];
 
 // export default preview;
