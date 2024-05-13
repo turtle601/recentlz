7028f1789c9603261a3a0bb47ff964ca3d8f7017
turtle601 <turtle601@naver.com>
2024-05-13 20:18:46 +0900

    📦 chore: msw mock 데이터 설정 [#17]

---
+++ client/src/mocks/video.ts
@@ -0,0 +1,13 @@
+export const videos = [
+  {
+    id: '1',
+    title: "NewJeans (뉴진스) 'OMG' Official MV",
+    videoId: 'gFJ-obHmtE4',
+    likeCount: 14000,
+    viewCount: 111111111,
+    totalCommentsCount: 1700000,
+    thumbnail:
+      'https://i.ytimg.com/vi/_ZAgIHmHLdc/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBp2eA5HiNFQoX82n4yEmiun-pW6A',
+    commentsIdList: ['1', '2'],
+  },
+];
