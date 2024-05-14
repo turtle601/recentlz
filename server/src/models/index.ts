b46cbe8a2f2b11e785bb84a0cd7de802cad193d1
turtle601 <turtle601@naver.com>
2024-05-14 12:40:33 +0900

    ✨ feat: Chart, VideoInfo 관련 스키마 생성 및 연결 [#19]

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,8 +1,10 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { CommentOrmType } from './../interface/db/comment';
 import dotenv from 'dotenv';
+import { Sequelize, DataTypes } from 'sequelize';
 
-import type { CommentOrmType } from '../interface/comments';
 import type { DBType } from '../interface/db';
+import type { VideoInfoOrmType } from '../interface/db/video';
+import { ChartOrmType } from '../interface/db/chart';
 
 dotenv.config();
 
@@ -16,18 +18,69 @@ const sequelize = new Sequelize(
   }
 );
 
+const VideoInfo = sequelize.define<VideoInfoOrmType>('videos', {
+  videoId: {
+    type: DataTypes.STRING,
+    unique: true,
+    primaryKey: true, // 비디오 ID를 기본 키로 설정
+  },
+  commentCount: {
+    type: DataTypes.INTEGER,
+  },
+  likeCount: {
+    type: DataTypes.INTEGER,
+  },
+  thumbnail: {
+    type: DataTypes.STRING,
+  },
+  viewCount: {
+    type: DataTypes.INTEGER,
+  },
+});
+
+const Comment = sequelize.define<CommentOrmType>('comment', {
+  videoId: {
+    type: DataTypes.STRING,
+  },
+  commentsIdList: {
+    type: DataTypes.TEXT('long'),
+  },
+});
+
+const Chart = sequelize.define<ChartOrmType>('chart', {
+  videoId: {
+    type: DataTypes.STRING,
+  },
+  removed: {
+    type: DataTypes.INTEGER,
+  },
+  viewCount: {
+    type: DataTypes.INTEGER,
+  },
+});
+
+VideoInfo.hasOne(Comment, {
+  foreignKey: 'videoId',
+});
+
+Comment.belongsTo(VideoInfo, {
+  foreignKey: 'videoId',
+});
+
+VideoInfo.hasOne(Chart, {
+  foreignKey: 'videoId',
+});
+
+Chart.belongsTo(VideoInfo, {
+  foreignKey: 'videoId',
+});
+
 const DB: DBType = {
-  Sequelize,
   sequelize,
-  Comments: sequelize.define<CommentOrmType>('comment', {
-    videoId: {
-      type: DataTypes.STRING,
-      unique: true,
-    },
-    commentsIdList: {
-      type: DataTypes.TEXT('long'),
-    },
-  }),
+  Sequelize,
+  VideoInfo,
+  Comment,
+  Chart,
 };
 
 export default DB;
